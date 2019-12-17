b163cbf84a550168d5bd15a45d79329db0542833
Podoliak Artem <whiteworker777@gmail.com>
2019-12-18 01:00:19 +0200

    added write message test

--- ServerTest.py
+++ ServerTest.py
@@ -9,25 +9,40 @@ process = threading.Thread(target=server.run_Datanode,args=[5001,1])
 process.daemon=True
 process.start()
 time.sleep(1)
-process = threading.Thread(target=server.run_Datanode,args=[5002,2])
-process.daemon=True
-process.start()
-time.sleep(1)
 
 class ServerTest(unittest.TestCase):
-    def test_create_queue(self):
-        r=requests.post("http://localhost:5001/create_queue", json={"qname":"A"})
-        self.assertEqual(r.content[:-1], b'{"qname":"A"}')
-        r=requests.post("http://localhost:5001/create_queue", json={"qname":"B"})
-        self.assertEqual(r.content[:-1], b'{"qname":"B"}')
-        r=requests.post("http://localhost:5001/get_queues", json={})
-        self.assertEqual(r.content[:-1], b'{"A":[],"B":[]}')
+    #def test_create_queue(self):
+    #    r=requests.post("http://localhost:5001/create_queue", json={"qname":"A"})
+    #    self.assertEqual(r.content[:-1], b'{"qname":"A"}')
+    #    r=requests.post("http://localhost:5001/create_queue", json={"qname":"B"})
+    #    self.assertEqual(r.content[:-1], b'{"qname":"B"}')
+    #    r=requests.post("http://localhost:5001/get_queues", json={})
+    #    self.assertEqual(r.content[:-1], b'{"A":[],"B":[]}')
     
-    def test_delete_queue(self):
-        r=requests.post("http://localhost:5001/delete_queue", json={"qname":"A"})
-        self.assertEqual(r.content, b'deleted')
+    #def test_delete_queue(self):
+    #    r=requests.post("http://localhost:5001/delete_queue", json={"qname":"A"})
+    #    self.assertEqual(r.content, b'deleted')
+    #    r=requests.post("http://localhost:5001/get_queues", json={})
+    #    self.assertEqual(r.content[:-1], b'{"B":[]}')
+
+    def test_write_msg(self):
+        r=requests.post("http://localhost:5001/create_queue", json={"qname":"A"})
+        r=requests.post("http://localhost:5001/create_queue", json={"qname":"B"})
+
+        r=requests.post("http://localhost:5001/write_message", json={"qname":"A","message":"A1"})
+        self.assertEqual(r.content[:-1], b'{"message":"A1","qname":"A"}')
+
+        r=requests.post("http://localhost:5001/write_message", json={"qname":"A","message":"A2"})
+        self.assertEqual(r.content[:-1], b'{"message":"A2","qname":"A"}')
+
         r=requests.post("http://localhost:5001/get_queues", json={})
-        self.assertEqual(r.content[:-1], b'{"B":[]}')
+        self.assertEqual(r.content[:-1], b'{"A":["A1","A2"],"B":[]}')
+
+        r=requests.post("http://localhost:5001/write_message", json={"qname":"B","message":"B1"})
+        self.assertEqual(r.content[:-1], b'{"message":"B1","qname":"B"}')
+
+        r=requests.post("http://localhost:5001/get_queues", json={})
+        self.assertEqual(r.content[:-1], b'{"A":["A1","A2"],"B":["B1"]}')
 
 unittest.main()
 
