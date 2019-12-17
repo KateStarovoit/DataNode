86c702b866498fd2da1406c6a182c2799fde90e2
AndriyPyzh <57706635+AndriyPyzh@users.noreply.github.com>
2019-12-17 19:02:29 +0200

    Update run.py
    Example how to run servers

--- run.py
+++ run.py
@@ -3,8 +3,12 @@ import json
 import time
 import threading
 from datetime import datetime
-import ser
+import server1
 
-process = threading.Thread(target=ser.run_Datanode,args=[5001,1])
+process = threading.Thread(target=server1.run_Datanode,args=[5001,1])
+process.start()
+process = threading.Thread(target=server1.run_Datanode,args=[5002,2])
+process.start()
+process = threading.Thread(target=server1.run_Datanode,args=[5003,3])
 process.start()
 
