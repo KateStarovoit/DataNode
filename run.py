7d7226e813fa99f5d29576f510747379e95d104c
AndriyPyzh <57706635+AndriyPyzh@users.noreply.github.com>
2019-12-17 20:45:45 +0200

    Update run.py
    bug fixed

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ import json
 import time
 import threading
 from datetime import datetime
-import server1
+import server2
 
-process = threading.Thread(target=server1.run_Datanode,args=[5001,1])
+process = threading.Thread(target=server2.run_Datanode,args=[5001,1])
 process.start()
-process = threading.Thread(target=server1.run_Datanode,args=[5002,2])
+time.sleep(1)
+process = threading.Thread(target=server2.run_Datanode,args=[5002,2])
 process.start()
-process = threading.Thread(target=server1.run_Datanode,args=[5003,3])
+time.sleep(1)
+process = threading.Thread(target=server2.run_Datanode,args=[5003,3])
 process.start()
-
